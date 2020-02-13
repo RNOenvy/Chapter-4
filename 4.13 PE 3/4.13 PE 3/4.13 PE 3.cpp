df27f630fe2fa1956f3e2eeaca2cc1de0ec4ba2a
Dan <ninetythreemedia@gmail.com>
2020-02-12 19:59:07 -0800

    Chapter 4

---
+++ 4.13 PE 3/4.13 PE 3/4.13 PE 3.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	
+	int day, age, month;
+	cout << "Enter your age followed by the month";
+		cin >> age >> month;
+		day = (age * 365) + (month * 30);
+		cout << "your age in days:\n";
+		cout << day;
+
+		return 0;
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
